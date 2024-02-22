5652887897faf008ef12015f35d93058a2a44786
Dan Morriss <danielmorriss1@gmail.com>
2024-02-22 16:36:11 +0000

    Add landing/about us page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import About from "./pages/about/About";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Discover Page</h1>} />
-          <Route exact path="/about" render={() => <h1>Landing Page</h1>} />
+          <Route exact path="/about" render={() => <About />} />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <h1>Page not found</h1>} />
---
+++ src/pages/about/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import styles from "../../styles/About.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom/cjs/react-router-dom";
+
+const About = () => {
+  const currentUser = useCurrentUser();
+  return (
+    <div className={styles.Background}>
+      <Card className="text-center p-3 shadow-lg" style={{ width: "40rem" }}>
+        <Card.Body>
+          <h1 className={styles.Handwritten}>Highlights</h1>
+          <Card.Text className="text-muted pt-3">
+            The daily companion for sharing the highlights of your day in just a
+            few minutes.
+          </Card.Text>
+          <h2 className={`${styles.Heading2} text-muted`}>
+            What was the best part of your day?
+          </h2>
+          <Card.Text className="text-muted pt-2">
+            Practicing gratitude to boost positivity, reduce anxiety, and
+            improve well-being. It's the simplest, science-backed way to a
+            happier you.
+          </Card.Text>
+          {currentUser ? (
+            <Link to="/contact-us"><Button className={styles.Button} variant="primary">
+              Send us a Message
+            </Button></Link>
+          ) : (
+            <>
+              <Link to="/signup">
+                <Button className={styles.Button} variant="primary">
+                  Sign Up
+                </Button>
+              </Link>
+              <Link to="/signin">
+                <Button className={styles.Button} variant="primary">
+                  Sign In
+                </Button>
+              </Link>
+            </>
+          )}
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default About;
