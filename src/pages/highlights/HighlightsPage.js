3b045d4fe599a69bc398b6ab18fc72ca949acee5
Dan Morriss <danielmorriss1@gmail.com>
2024-02-29 13:29:26 +0000

    Add logic to filter highlights for discover, feed and liked pages

--- src/pages/highlights/HighlightsPage.js
+++ src/pages/highlights/HighlightsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -7,14 +7,57 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/HighlightsPage.module.css";
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import Highlight from "./Highlight";
 
-function HighlightsPage() {
-  
+import NoResults from "../../assets/no-results.png";
+import Asset from "../../components/Asset";
+
+function HighlightsPage({ message, filter = "" }) {
+  const [highlights, setHighlights] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchHighlights = async () => {
+      try {
+        const { data } = await axiosReq.get(`/highlights/?${filter}`);
+        setHighlights(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    setHasLoaded(false);
+    fetchHighlights();
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        {hasLoaded ? (
+          <>
+            {highlights.results.length ? (
+              highlights.results.map((highlight) => (
+                <Highlight
+                  key={highlight.id}
+                  {...highlight}
+                  setHighlights={setHighlights}
+                />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
