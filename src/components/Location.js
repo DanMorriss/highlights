b15101fdf450c7f9f8b8c47ef1ed519ee57311c2
Dan Morriss <danielmorriss1@gmail.com>
2024-03-13 14:40:49 +0000

    Add loaction data to API in Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import btnStyles from "../styles/Buttons.module.css";
 import { axiosReq } from "../api/axiosDefaults";
 import Asset from "./Asset";
+import { Form } from "react-bootstrap";
 
 const Location = () => {
   const [errors, setErrors] = useState({});
@@ -12,32 +13,49 @@ const Location = () => {
   });
   const [locationLoading, setLocationLoading] = useState(false);
 
-  const getLocation = () => {
+  /**
+   * This function retrieves the current location using the browser's geolocation API,
+   * fetches the reverse geocode information from a third-party API,
+   * and updates the location data in the component state.
+   * If an error occurs, logs the error.
+   */
+  const getLocation = async () => {
     setLocationLoading(true);
-    const currentLocation = navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const lat = position.coords.latitude;
-        const long = position.coords.longitude;
-        const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`;
-        fetch(locationApi)
-          .then((response) => response.json())
-          .then((data) => {
-            setLocationData({
-              latitude: lat,
-              longitude: long,
-              name: data.city + ", " + data.countryCode,
-            });
-            console.log(locationData);
-            setLocationLoading(false);
-            // handleSubmit(locationData);
-          });
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
+    try {
+      const position = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      });
+
+      const { latitude, longitude } = position.coords;
+      const locationApi = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+      const response = await fetch(locationApi);
+      const data = await response.json();
+
+      setLocationData({
+        latitude,
+        longitude,
+        name: data.city + ", " + data.countryCode,
+      });
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLocationLoading(false);
+    }
   };
 
+  {/* Add location to database */}
+  useEffect(() => {
+    if (locationData) {
+      console.log(locationData);
+      handleSubmit(locationData);
+    }
+  }, [locationLoading]);
+
+  /**
+   * Handle form submission asynchronously.
+   * 
+   * @return {Promise} a Promise that resolves when the submission is handled
+   */
   const handleSubmit = async (e) => {
     try {
       const { data } = await axiosReq.post("/locations/", locationData);
@@ -53,16 +71,21 @@ const Location = () => {
         className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
         onClick={getLocation}
       >
-        Add Current Location
+        <i class="fa-solid fa-location-dot"></i>
       </div>
+      <Form>
+        <Form.Group>
+          <Form.Label>Add Location</Form.Label>
+          <Form.Control
+            type="text"
+            value={locationData.name}
+
+            onChange={(e) => setLocationData({ ...locationData, name: e.target.value })}
+          />
+        </Form.Group>
+      </Form>
       <div>
-        {locationLoading && (
-          <Asset spinner />
-        )}
-        {locationData.name && (
-          <p>{locationData.name}</p>
-        )}
-        
+        {locationLoading && <Asset spinner />}
       </div>
     </>
   );
