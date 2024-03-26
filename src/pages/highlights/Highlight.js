9bb8058840377c59740409adb0e9402284470946
Dan Morriss <danielmorriss1@gmail.com>
2024-03-26 15:10:17 +0000

    Change spinner style and color and add lined paper effect to highlights

--- src/pages/highlights/Highlight.js
+++ src/pages/highlights/Highlight.js
@@ -120,135 +120,135 @@ const Highlight = (props) => {
 
   return (
     <Card className={`${styles.Highlight}`}>
-      <Card.Body>
-        <Media className="align-items-center justify-content-between">
-          {/* Image link to users profile */}
-          <Link className={`${styles.User}`} to={`/profiles/${profile_id}`}>
-            <Avatar src={profile_image} height={55} />
-            {owner}
+      <div className={styles.Paper}>
+        <Card.Body>
+          <Media className="align-items-center justify-content-between">
+            {/* Image link to users profile */}
+            <Link className={`${styles.User}`} to={`/profiles/${profile_id}`}>
+              <Avatar src={profile_image} height={55} />
+              {owner}
+            </Link>
+
+            {/* Date link to the highlight page. Does not display on the highlight page */}
+            {!highlightPage ? (
+              <div className="d-flex align-items-center">
+                <Link
+                  className="align-items-center justify-content-between"
+                  to={`/highlights/${id}`}
+                >
+                  <div className="d-flex align-items-center">
+                    <span className={`${styles.DiaryFontLinkStandardDisplay} `}>
+                      {formattedDate}
+                    </span>
+                  </div>
+                </Link>
+                {/* Dropdown menu for editing and deleting highlights if the user is the owner */}
+                {is_owner && (
+                  <MoreDropdown
+                    handleEdit={handleEdit}
+                    handleDelete={handleDelete}
+                  />
+                )}
+              </div>
+            ) : (
+              // Displays the date on the highlight page
+              <div className="d-flex align-items-center">
+                <span className={`${styles.DiaryFont}`}>{formattedDate}</span>
+                {/* Dropdown menu for editing and deleting highlights if the user is the owner */}
+                {is_owner && highlightPage && (
+                  <MoreDropdown
+                    handleEdit={handleEdit}
+                    handleDelete={handleDelete}
+                  />
+                )}
+              </div>
+            )}
+          </Media>
+        </Card.Body>
+
+        {/* If there is an image, display it as a link to the highlight page */}
+        {image && (
+          <Link to={`/highlights/${id}`}>
+            <Card.Img src={image} alt={title} />
           </Link>
-
-          {/* Date link to the highlight page. Does not display on the highlight page */}
-          {!highlightPage ? (
-            <div className="d-flex align-items-center">
-              <Link
-                className="align-items-center justify-content-between"
-                to={`/highlights/${id}`}
-              >
-                <div className="d-flex align-items-center">
-                  <span className={`${styles.DiaryFontLinkStandardDisplay} `}>
-                    {formattedDate}
-                  </span>
-                </div>
-              </Link>
-              {/* Dropdown menu for editing and deleting highlights if the user is the owner */}
-              {is_owner && (
-                <MoreDropdown
-                  handleEdit={handleEdit}
-                  handleDelete={handleDelete}
-                />
-              )}
-            </div>
-          ) : (
-            // Displays the date on the highlight page
-            <div className="d-flex align-items-center">
-              <span className={`${styles.DiaryFont}`}>{formattedDate}</span>
-              {/* Dropdown menu for editing and deleting highlights if the user is the owner */}
-              {is_owner && highlightPage && (
-                <MoreDropdown
-                  handleEdit={handleEdit}
-                  handleDelete={handleDelete}
-                />
-              )}
-            </div>
-          )}
-        </Media>
-      </Card.Body>
-
-      {/* If there is an image, display it as a link to the highlight page */}
-      {image && (
-        <Link
-          to={`/highlights/${id}`}
-        >
-          <Card.Img src={image} alt={title} />
-        </Link>
-      )}
-
-      <Card.Body>
-        {/* Displays the location on the highlight page */}
-        {location && highlightPage && (
-          <span className={`${styles.DiaryFont} text-muted`}>{location}</span>
-        )}
-        <div className="d-flex justify-content-between align-items-baseline mb-3">
-          {/* Displays the category on the highlight page */}
-          {category && highlightPage && (
-            <span className={styles.Category}>#{category}</span>
-          )}
-          {/* Displays the tagged user on the highlight page if there is one */}
-          {tagged_user && highlightPage && (
-            <Avatar src={tagged_user.profile_image} height={30} />
-          )}
-        </div>
-
-        {/* Displays the title question on the highlight page */}
-        {highlightPage && (
-          <Card.Title className={`${styles.DiaryQuestions} mb-3`}>
-            What was the highlight of your day?
-          </Card.Title>
         )}
 
-        <p className={`${styles.DiaryFont} text-center `}>{title}</p>
+        <Card.Body>
+          {/* Displays the location on the highlight page */}
+          {location && highlightPage && (
+            <span className={`${styles.DiaryFont} text-muted`}>{location}</span>
+          )}
+          <div className="d-flex justify-content-between align-items-baseline mb-3">
+            {/* Displays the category on the highlight page */}
+            {category && highlightPage && (
+              <span className={styles.Category}>#{category}</span>
+            )}
+            {/* Displays the tagged user on the highlight page if there is one */}
+            {tagged_user && highlightPage && (
+              <Avatar src={tagged_user.profile_image} height={30} />
+            )}
+          </div>
 
-        {/* Displays the description on the highlight page if there is one */}
-        {description && highlightPage && (
-          <Card.Text className="text-center">{description}</Card.Text>
-        )}
-
-        {/* Displays the improve on the highlight page if there is one */}
-        {improve && highlightPage && (
-          <>
+          {/* Displays the title question on the highlight page */}
+          {highlightPage && (
             <Card.Title className={`${styles.DiaryQuestions} mb-3`}>
-              What would have made today even better?
+              What was the highlight of your day?
             </Card.Title>
-            <Card.Text className="text-center">{improve}</Card.Text>
-          </>
-        )}
-
-        <div className={` d-flex justify-content-center align-items-center`}>
-          {/* If the user is the owner, let them know they can't like their own highlight */}
-          {is_owner ? (
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>You can't like your own highlight</Tooltip>}
-            >
-              <i className="far fa-heart"></i>
-            </OverlayTrigger>
-          ) : // If the user has liked the highlight, display the filled in heart
-          like_id ? (
-            <span onClick={handleUnlike}>
-              <i className={`fas fa-heart ${styles.Heart}`} />
-            </span>
-          ) : // If the user has not liked the highlight and is logged in, display the outline
-          currentUser ? (
-            <span onClick={handleLike}>
-              <i className={`far fa-heart ${styles.HeartOutline}`} />
-            </span>
-          ) : (
-            // If the user is not logged in, display the outline and tell them to log in to like a highlight
-            <OverlayTrigger
-              placement="top"
-              overlay={<Tooltip>Log in to like a highlight</Tooltip>}
-            >
-              <i className="far fa-heart"></i>
-            </OverlayTrigger>
           )}
-          {likes_count}
-          <Link className={styles.Comment} to={`/highlights/${id}`}>
-            <i className="far fa-comment" />
-          </Link>
-          {comments_count}
-        </div>
-      </Card.Body>
+
+          <p className={`${styles.DiaryFont} text-center `}>{title}</p>
+
+          {/* Displays the description on the highlight page if there is one */}
+          {description && highlightPage && (
+            <Card.Text className="text-center">{description}</Card.Text>
+          )}
+
+          {/* Displays the improve on the highlight page if there is one */}
+          {improve && highlightPage && (
+            <>
+              <Card.Title className={`${styles.DiaryQuestions} mb-3`}>
+                What would have made today even better?
+              </Card.Title>
+              <Card.Text className="text-center">{improve}</Card.Text>
+            </>
+          )}
+
+          <div className={` d-flex justify-content-center align-items-center`}>
+            {/* If the user is the owner, let them know they can't like their own highlight */}
+            {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't like your own highlight</Tooltip>}
+              >
+                <i className="far fa-heart"></i>
+              </OverlayTrigger>
+            ) : // If the user has liked the highlight, display the filled in heart
+            like_id ? (
+              <span onClick={handleUnlike}>
+                <i className={`fas fa-heart ${styles.Heart}`} />
+              </span>
+            ) : // If the user has not liked the highlight and is logged in, display the outline
+            currentUser ? (
+              <span onClick={handleLike}>
+                <i className={`far fa-heart ${styles.HeartOutline}`} />
+              </span>
+            ) : (
+              // If the user is not logged in, display the outline and tell them to log in to like a highlight
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Log in to like a highlight</Tooltip>}
+              >
+                <i className="far fa-heart"></i>
+              </OverlayTrigger>
+            )}
+            {likes_count}
+            <Link className={styles.Comment} to={`/highlights/${id}`}>
+              <i className="far fa-comment" />
+            </Link>
+            {comments_count}
+          </div>
+        </Card.Body>
+      </div>
     </Card>
   );
 };
